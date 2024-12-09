26ce7af83f19475b45a5c7f2cf7d8e728f6d7434
Anamul Haque <138350161+Anamul9901@users.noreply.github.com>
2024-12-10 00:14:11 +0600

    update

--- prisma/migrations/20241208134100_update/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "course_classes" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20241209172942_update_grade/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "grades" ADD COLUMN     "grade" DOUBLE PRECISION,
-ADD COLUMN     "remarks" TEXT;
